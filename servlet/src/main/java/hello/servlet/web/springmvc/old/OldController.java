5ffab553a470cd0854dfb59ac26b28599c38bd7d
IMWoo94 <leemeawoo94@gmail.com>
2023-09-18 14:34:31 +0900

    spring mvc old springmvc interface controller and interface httprequestHandler 2023.09.18

---
+++ servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,22 @@
+package hello.servlet.web.springmvc.old;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+/**
+ * 옛날 버전의 Controller
+ * spring bean의 이름을 url 패턴으로 맞춘다.
+ */
+@Component("/springmvc/old-controller")
+public class OldController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("OldController.handleRequest");
+		return null;
+	}
+}
