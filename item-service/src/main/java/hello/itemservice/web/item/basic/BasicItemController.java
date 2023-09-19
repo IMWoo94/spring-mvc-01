365e763fb41053e5cb535303454336dbcbaa08f6
IMWoo94 <leemeawoo94@gmail.com>
2023-09-19 15:16:22 +0900

    spring mvc item detail page add 2023.09.19

--- item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import javax.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import hello.itemservice.domain.item.Item;
@@ -20,13 +21,6 @@ public class BasicItemController {
 
 	private final ItemRepository itemRepository;
 
-	@GetMapping
-	public String items(Model model) {
-		List<Item> items = itemRepository.findAll();
-		model.addAttribute("items", items);
-		return "basic/items";
-	}
-
 	/**
 	 * 테스트용 데이터 추가
 	 */
@@ -35,4 +29,21 @@ public class BasicItemController {
 		itemRepository.save(new Item("testA", 10000, 10));
 		itemRepository.save(new Item("testB", 20000, 20));
 	}
+
+	@GetMapping
+	public String items(Model model) {
+		List<Item> items = itemRepository.findAll();
+		model.addAttribute("items", items);
+		return "basic/items";
+	}
+
+	@GetMapping("/{itemId}")
+	public String item(@PathVariable Long itemId, Model model) {
+		Item item = itemRepository.findById(itemId);
+
+		model.addAttribute("item", item);
+		return "basic/item";
+
+	}
+
 }
